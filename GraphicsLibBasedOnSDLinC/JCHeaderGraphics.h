0d39f2b73e1208088e8b1628502a656aa28444a1
JConeFun <34948071+JConeFun@users.noreply.github.com>
2018-01-08 10:12:46 -0800

    Add project files.

---
+++ GraphicsLibBasedOnSDLinC/JCHeaderGraphics.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef LEDGENDS_H
+#define LEDGENDS_H
+#include "include\SDL.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#endif
---
+++ GraphicsLibBasedOnSDLinC/Source.c
@@ -0,0 +1,13 @@
+#include "JCHeaderGraphics.h"
+
+int main()
+{
+	int i = 0;
+
+	while (i < 6)
+	{
+		printf("hi");
+		++i;
+	}
+	return (0);
+}
