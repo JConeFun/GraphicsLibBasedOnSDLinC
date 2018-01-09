0cd038f89086d072918b08d5c38f289a25d50cd3
JConeFun <34948071+JConeFun@users.noreply.github.com>
2018-01-08 18:46:49 -0800

    Added mising peices to run and have a window open up.

--- GraphicsLibBasedOnSDLinC/Source.c
+++ GraphicsLibBasedOnSDLinC/Source.c
@@ -4,10 +4,32 @@ int main()
 {
 	int i = 0;
 
-	while (i < 6)
+	SDL_Window		*win;
+	SDL_Renderer	*ren;
+	SDL_Surface		*frame_screen_shot_1;
+	SDL_Surface		*load_surf;
+	SDL_Texture		*bmp_text;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		return (1);
+	win = SDL_CreateWindow("Testing", 10, 40, 1425, 800, SDL_WINDOW_SHOWN);
+	if (win == NULL)
 	{
-		printf("hi");
-		++i;
+		printf("testing  123");
+		getchar();
+		return (1);
 	}
+	
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == NULL)
+		return (1);
+	SDL_SetRenderDrawColor(ren, 0, 0, 255, 0);
+
+	SDL_RenderPresent(ren);
+	SDL_RenderClear(ren);
+	SDL_Delay(10000);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	getchar();
 	return (0);
 }
